c10c4653780473584b39d16083e7d7a5c0e7306d
holly <h.person.notrobot@gmail.com>
2024-10-03 22:48:30 +0100

    Removed header from data.csv

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 # Main.py
 #
 from Bounce import Bounce
+from dataLoading import readData
 
 def main():
     print("Some processing stuff ig")
+    readData()
     bounce = Bounce()
 
 if __name__ == "__main__":
