9594c442b213cd49445a8faaa25b6dd02599b033
holly <h.person.notrobot@gmail.com>
2024-10-03 22:32:14 +0100

    Initial commit with Bounce class.

---
+++ Bounce.py
@@ -0,0 +1,35 @@
+import numpy as np
+#TODO: further comment the values in this class.
+class Bounce:
+    # --- Varibles from dataset: ---
+
+    #overNumber = 0                 # Over number, unused.
+    #ballNumber = 0                 # Ball number, unused.
+    #bowlerName = ""                # Bowler name, Unused.
+    #batsmansHand = True            # Hand used, Unused. True for right, False for left.
+    bowlerReleaseSpeed = 0.0        # float
+
+    # Note: All variables with "Pos" and compressions of their corresponding 1D values in the dataset.
+    # Dimensions will be spesified next to them respectively.
+    # Acceleration also follows this rule.
+    bouncePos = np.array([0.0, 0.0])# X,Y
+    stumpsPos = np.array([0.0, 0.0])# Y,Z
+    swing = 0.0
+    deviation = 0.0
+    #runsScored = NaN               # Unused(?)
+    shotLandingPos = np.array([0.0, 0.0])# X,Y
+    #trajectoryTime = 0             # Unused. Should not be a number?
+    #trajectoryDate = 0             # Unused. Also not a number.
+    bounceVelocity = 0.0            # Bounce velocity.
+    outOfBounceAngle = 0.0
+    dropAngle = 0.0
+    angleLeavingBowlersHand = 0.0   # Come up with better variable names istg.
+    #shotPlayed = ""                # Unused.
+    #shotType = ""                  # Unused.
+    bowlerReleasePos = np.array([0.0,0.0])# Yposition,Zposition (Pick a the damn naming scheme)
+    acceleration = np.array([0.0, 0.0])#Y,Z
+
+    # --- ---
+    def __init__(self):
+        print("Hello, world!")
+
---
+++ main.py
@@ -0,0 +1,11 @@
+# Main.py
+#
+from Bounce import Bounce
+
+def main():
+    print("Some processing stuff ig")
+    bounce = Bounce()
+
+if __name__ == "__main__":
+    main()
+
