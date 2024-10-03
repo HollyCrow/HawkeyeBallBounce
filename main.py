5979e61ef4979d910391c164a088993ad72cc7dc
holly <h.person.notrobot@gmail.com>
2024-10-03 23:14:37 +0100

    Updated data.csv to remove problematic encoding.

--- main.py
+++ main.py
@@ -3,11 +3,12 @@
 from Bounce import Bounce
 from dataLoading import readData
 
+
 def main():
     print("Some processing stuff ig")
     readData()
     bounce = Bounce()
 
+
 if __name__ == "__main__":
     main()
-
